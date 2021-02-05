7ccd6ec6a82f3502b343258be5c64e995e4f9c8c
M. Ochoa <40745550+mochoabuilds@users.noreply.github.com>
2021-02-05 15:54:02 -0600

    Update HomeController.swift

--- ConnieDelivers/Controller/HomeController.swift
+++ ConnieDelivers/Controller/HomeController.swift
@@ -10,17 +10,19 @@ import Firebase
 import MapKit
 
 private let reuseIdentifer = "LocationCell"
+private let annotationIdentifier = "DriverAnnotation"
 
 class HomeController: UIViewController {
     
     // MARK: - Properties
     
     private let mapView = MKMapView()
-    private let locationManager = CLLocationManager()
+    private let locationManager = LocationHandler.shared.locationManager
     
     private let inputActivationView = LocationInputActivationView()
     private let locationInputView = LocationInputView()
     private let tableView = UITableView()
+    private var searchResults = [MKPlacemark]()
     
     private var user: User? {
         didSet { locationInputView.user = user }
@@ -34,18 +36,45 @@ class HomeController: UIViewController {
         super.viewDidLoad()
         checkIfUserIsLoggedIn()
         enableLocationServices()
-        fetchUserData()
 //        signOut()
     }
     
     // MARK: - API
     
     func fetchUserData() {
-        Service.shared.fetchUserData { user in
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        Service.shared.fetchUserData(uid: currentUid) { user in
             self.user = user
         }
     }
     
+    // display driver on map
+    func fetchDrivers() {
+        guard let location = locationManager?.location else { return }
+        Service.shared.fetchDriver(location: location) { (driver) in
+            guard let coordinate = driver.location?.coordinate else { return }
+            let annotation = DriverAnnotation(uid: driver.uid, coordinate: coordinate)
+            
+            // determining if driver is already visble
+            var driverIsVisble: Bool {
+                return self.mapView.annotations.contains { annotation -> Bool in
+                    guard let driverAnno = annotation as? DriverAnnotation else { return false }
+                    
+                    if driverAnno.uid == driver.uid {
+                        driverAnno.updateAnnotationPosition(withCoordinate: coordinate)
+                        return true
+                    }
+                    return false
+                }
+            }
+            // adding driver to map if not visble
+            if !driverIsVisble {
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+
+    // check if guest logged in
     func checkIfUserIsLoggedIn() {
         if Auth.auth().currentUser?.uid == nil {
             DispatchQueue.main.async {
@@ -53,19 +82,29 @@ class HomeController: UIViewController {
                 self.present(nav, animated: true, completion: nil)
             }
         } else {
-           configureUI()
+           configure()
         }
     }
-    
+        // sign out
         func signOut() {
             do {
                 try Auth.auth().signOut()
+                DispatchQueue.main.async {
+                    let nav = UINavigationController(rootViewController: LoginController())
+                    self.present(nav, animated: true, completion: nil)
+                }
             } catch {
                 print("FIX ME: Error signing out")
             }
         }
     
         // MARK: - Helper Functions
+    
+        func configure() {
+            configureUI()
+            fetchUserData()
+            fetchDrivers()
+        }
     
         func configureUI() {
             configureMapView()
@@ -84,21 +123,23 @@ class HomeController: UIViewController {
             configureTableView()
         }
 
-            func configureMapView() {
-                view.addSubview(mapView)
-                mapView.frame = view.frame
+        func configureMapView() {
+            view.addSubview(mapView)
+            mapView.frame = view.frame
             
-                mapView.showsUserLocation = true
-                mapView.userTrackingMode = .follow
-    }
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+            mapView.delegate = self
+        }
     
-    func configureLocationInputView() {
-        locationInputView.delegate = self
-        view.addSubview(locationInputView)
-        locationInputView.anchor(top: view.topAnchor, left: view.leftAnchor,
+        func configureLocationInputView() {
+            locationInputView.delegate = self
+            view.addSubview(locationInputView)
+            locationInputView.anchor(top: view.topAnchor, left: view.leftAnchor,
                                  right: view.rightAnchor, height: 200)
-        locationInputView.alpha = 0
+            locationInputView.alpha = 0
         
+        // dynamic animations
         UIView.animate(withDuration: 0.5, animations: {
             self.locationInputView.alpha = 1
         }) { _ in
@@ -121,40 +162,81 @@ class HomeController: UIViewController {
                                  width: view.frame.width, height: height)
         
         view.addSubview(tableView)
-        
+    }
+    
+    func dismissLocationView(completion: ((Bool) -> Void)? = nil) {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.locationInputView.alpha = 0
+            self.tableView.frame.origin.y = self.view.frame.height
+            // Lightens Computational Load
+            self.locationInputView.removeFromSuperview()
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.inputActivationView.alpha = 1
+            })
+        }, completion: completion)
     }
 }
 
-// MARK: - Location Services
+// MARK: - Map Helper Functions
 
-extension HomeController: CLLocationManagerDelegate {
-    func enableLocationServices() {
-        locationManager.delegate = self
+private extension HomeController {
+    func searchBy(naturalLanguageQuery: String, completion: @escaping([MKPlacemark]) -> Void) {
+        var results =  [MKPlacemark]()
         
+        let request = MKLocalSearch.Request()
+        request.region = mapView.region
+        request.naturalLanguageQuery = naturalLanguageQuery
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response else { return }
+            
+            response.mapItems.forEach({ item in
+                results.append(item.placemark)
+            })
+            completion(results)
+        }
+    }
+}
+
+
+// MARK: - MKMapViewDelegate
+
+extension HomeController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? DriverAnnotation {
+            let view = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier )
+            view.image = #imageLiteral(resourceName: "chevron-sign-to-right")
+            return view
+        }
+        return nil
+    }
+    
+}
+
+// MARK: - LocationServices
+
+extension HomeController {
+    func enableLocationServices() {
+        // FIX ME: authorizationStatus()' was deprecated in iOS 14.0
         switch CLLocationManager.authorizationStatus(){
         case .notDetermined:
             print("FIX ME: Not determined...")
-            locationManager.requestWhenInUseAuthorization()
+            locationManager?.requestWhenInUseAuthorization()
         case .restricted, .denied:
             break
         case .authorizedAlways:
             print("FIX ME: Auth always...")
-            locationManager.startUpdatingLocation()
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager?.startUpdatingLocation()
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
         case .authorizedWhenInUse:
             print("FIX ME: Auth when in use...")
-            locationManager.requestAlwaysAuthorization()
+            locationManager?.requestAlwaysAuthorization()
         @unknown default:
             break
         }
-    }
-        
-        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status:
-            CLAuthorizationStatus) {
-            
-            if status == .authorizedWhenInUse {
-                locationManager.requestAlwaysAuthorization()
-            }
     }
 }
 
@@ -172,19 +254,15 @@ extension HomeController: LocationInputActivationViewDelegate {
 // MARK: - LocationInputViewDelegate
 
 extension HomeController: LocationInputViewDelegate {
-    func dismissLocationInputView() {
-        
-
-        UIView.animate(withDuration: 0.3, animations: {
-            self.locationInputView.alpha = 0
-            self.tableView.frame.origin.y = self.view.frame.height
-        }) { _ in
-            // Lightens Computational Load
-            self.locationInputView.removeFromSuperview()
-            UIView.animate(withDuration: 0.3, animations: {
-                self.inputActivationView.alpha = 1
-            })
+    func exectureSearch(query: String) {
+        searchBy(naturalLanguageQuery: query) { (results) in
+            self.searchResults = results
+            self.tableView.reloadData()
         }
+    }
+    
+    func dismissLocationInputView() {
+       dismissLocationView()
     }
 }
 
@@ -195,19 +273,32 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
         return "Test"
     }
     
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // If Section Equal to Zero Then Show 2, If Not Show 5
-        return section == 0 ? 2 : 5
+        return section == 0 ? 2 : searchResults.count
     }
     
-    //
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifer, for: indexPath) as!
             LocationCell
+        
+        if indexPath.section == 1 {
+            cell.placemark = searchResults[indexPath.row]
+        }
+    
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedPlacemark = searchResults[indexPath.row]
+        dismissLocationView { _ in
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = selectedPlacemark.coordinate
+            self.mapView.addAnnotation(annotation)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
     }
 }
