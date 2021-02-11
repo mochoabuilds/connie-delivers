e000592b168e9b2a6ae6fb486bcbc0dc83d710f9
M. Ochoa <40745550+mochoabuilds@users.noreply.github.com>
2021-02-11 14:20:32 -0600

    Create Trip.swift

---
+++ ConnieDelivers/Model/Trip.swift
@@ -0,0 +1,51 @@
+//
+//  Trip.swift
+//  ConnieDelivers
+//
+//  Created by M. Ochoa on 2/9/21.
+//
+
+//  Create trip object for db
+
+import CoreLocation
+
+// each case has raw values associated with action
+enum TripState: Int {
+    case requested
+    case accepted 
+    case inProgress
+    case completed
+}
+
+struct Trip {
+    
+    var pickupCoordinates: CLLocationCoordinate2D!
+    var destinationCoordinates: CLLocationCoordinate2D!
+    let passengerUid: String!
+    var driverUid: String?
+    var state: TripState!
+    
+    init(passengerUid: String, dictionary: [String: Any]) {
+        self.passengerUid = passengerUid
+        
+        if let pickupCoordinates = dictionary["pickupCoordinates"] as? NSArray {
+            guard let lat = pickupCoordinates[0] as? CLLocationDegrees else { return }
+            guard let long = pickupCoordinates[1] as? CLLocationDegrees else { return }
+            self.pickupCoordinates = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        }
+        
+        //
+        if let destinationCoordinates = dictionary["destinationCoordinates"] as? NSArray {
+            guard let lat = destinationCoordinates[0] as? CLLocationDegrees else { return }
+            guard let long = destinationCoordinates[1] as? CLLocationDegrees else { return }
+            self.destinationCoordinates = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        }
+        
+        self.driverUid = dictionary["driverUid"] as? String ?? ""
+        
+        if let state = dictionary["state"] as? Int {
+            self.state = TripState(rawValue: state)
+        }
+    }
+}
+
